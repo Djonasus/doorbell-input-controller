870b76706269cfabfa53da46f88008a3778d0dcc
Djonasus <60808389+Djonasus@users.noreply.github.com>
2025-01-27 18:08:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,23 +2,23 @@
 
 float BUZZER_PIN = 2;
 
-const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Four columns
+const byte ROWS = 4;
+const byte COLS = 4;
+
 char keys[ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {11, 10, 9, 8}; // Connect to the row pinouts of the keypad
-byte colPins[COLS] = {7, 6, 5, 4}; // Connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {11, 10, 9, 8};
+byte colPins[COLS] = {7, 6, 5, 4};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(50);
-  // welcome();
 }
 
 void loop() {
